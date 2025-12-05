7a24badbc9e45fdf833639eb067197d041d4270a
aandremeleti-hub <aandremeleti@gmail.com>
2025-12-05 01:36:17 -0300

    feat: create main section structure

--- src/layout/sections/main/index.jsx
+++ src/layout/sections/main/index.jsx
@@ -1,10 +1,24 @@
 import { CardMain } from '../../../components/cards/card-main'
 import './style.css'
+import iconeBotaoSeta from '../../../assets/images/home/icone_botao_seta.svg'
+import primaryImageMain from '../../../assets/images/home/primary_image_main.svg'
 
 export const Main = () => {
     return (
-        <section>
-            <CardMain parametro1="parametro1" parametro2="parametro2"/>
-        </section>
+        <main>
+            <div>
+                <div>
+                    <h1>Transformando gente em <span className='highlighted-word'>cliente</span></h1>
+                    <p>Estrutura digital simplificada que traz o cliente até você pronto para comprar</p>
+                    <div>
+                        <img src={iconeBotaoSeta} alt="" />
+                        <button>Contato</button>
+                    </div>
+                </div>
+                <img src={primaryImageMain} alt="" />
+            </div>
+
+            <CardMain parametro1="parametro1" parametro2="parametro2" />
+        </main>
     )
 }
