042cb6ba7f861eac6fae85b6228dee98d4d139ee
aandremeleti-hub <aandremeleti@gmail.com>
2025-12-06 13:06:29 -0300

    feat: create card design page

--- src/components/cards/card-design/index.jsx
+++ src/components/cards/card-design/index.jsx
@@ -1,8 +1,29 @@
-export const CardDesign = (parametro1, parametro2) => {
+import './style.css'
+
+export const CardDesign = ({ imagem, titulo, paragrafo, icone, item1, item2, item3, item4 }) => {
     return (
         <div>
-            <h1>{parametro1}</h1>
-            <h2>{parametro2}</h2>
+            <img src={imagem} alt="" />
+            <h1>{titulo}</h1>
+            <p>{paragrafo}</p>
+            <div>
+                <div>
+                    <img src={icone} alt="" />
+                    <p>{item1}</p>
+                </div>
+                <div>
+                    <img src={icone} alt="" />
+                    <p>{item2}</p>
+                </div>
+                <div>
+                    <img src={icone} alt="" />
+                    <p>{item3}</p>
+                </div>
+                <div>
+                    <img src={icone} alt="" />
+                    <p>{item4}</p>
+                </div>
+            </div>
         </div>
     )
 }
