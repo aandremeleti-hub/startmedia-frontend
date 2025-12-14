8036bd7d5ad7de9a13bdfac1212c2e0ac86a772b
aandremeleti-hub <aandremeleti@gmail.com>
2025-12-14 16:35:31 -0300

    feat: site reasons section structure creation

--- src/pages/site/index.jsx
+++ src/pages/site/index.jsx
@@ -1,12 +1,13 @@
 import { Header } from "../../components/layout/header"
 import { SiteMainSection } from "../../components/layout/site_main_section"
+import { SiteReasonsSection } from "../../components/layout/site_reasons_section"
 
 export const Site = () => {
     return (
         <>
             <Header />
             <SiteMainSection/>
-            
+            <SiteReasonsSection/>
         </>
     )
 }
