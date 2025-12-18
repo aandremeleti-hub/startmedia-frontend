75b95bb43c3bd9158a277cb4e42737870db2ac11
aandremeleti-hub <aandremeleti@gmail.com>
2025-12-18 20:08:35 -0300

    feat: ia primary cards section structure creation

--- src/components/layout/ia_primary_cards_section/index.jsx
+++ src/components/layout/ia_primary_cards_section/index.jsx
@@ -1,16 +1,28 @@
 import { CardIaPrimary } from '../../cards/card-ia-primary'
 import './style.css'
 import dashboard_ia_primary_cards_section from '../../../assets/images/home/dashboard_ia_primary_cards_section.svg'
-
+import robot_ia_primary_cards_section from '../../../assets/images/home/robot_ia_primary_cards_section.svg'
+import file_ia_primary_cards_section from '../../../assets/images/home/file_ia_primary_cards_section.svg'
 
 export const IaPrimaryCardsSection = () => {
     return (
         <section>
             <div className='container-ia-primary-cards'>
-                <CardIaPrimary
-                    imagem={dashboard_ia_primary_cards_section}
-                    titulo="Atendimento 24h"
-                    texto="O cliente consegue resolver tudo rápido, sem esperar, sem burocracia, sem repetir informações. Isso aumenta satisfação e conversão." />
+                <h1>O que você ganha?</h1>
+                <div>
+                    <CardIaPrimary
+                        imagem={dashboard_ia_primary_cards_section}
+                        titulo="Atendimento 24h"
+                        texto="O cliente consegue resolver tudo rápido, sem esperar, sem burocracia, sem repetir informações. Isso aumenta satisfação e conversão." />
+                    <CardIaPrimary
+                        imagem={robot_ia_primary_cards_section}
+                        titulo="Atendimento 24h"
+                        texto="O cliente consegue resolver tudo rápido, sem esperar, sem burocracia, sem repetir informações. Isso aumenta satisfação e conversão." />
+                    <CardIaPrimary
+                        imagem={file_ia_primary_cards_section}
+                        titulo="Atendimento 24h"
+                        texto="O cliente consegue resolver tudo rápido, sem esperar, sem burocracia, sem repetir informações. Isso aumenta satisfação e conversão." />
+                </div>
             </div>
         </section>
     )
