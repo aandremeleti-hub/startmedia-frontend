c62f4f5bd85148098a9c4347d6f2147591113f92
aandremeleti-hub <aandremeleti@gmail.com>
2025-12-18 00:17:26 -0300

    feat: site cases section structure creation

--- src/components/layout/site_cases_section/index.jsx
+++ src/components/layout/site_cases_section/index.jsx
@@ -3,20 +3,64 @@ import './style.css'
 import forbes_logo from '../../../assets/images/home/forbes_logo.svg'
 import icone_botao_seta from '../../../assets/images/home/icone_botao_seta.svg'
 import icon_ball from '../../../assets/images/home/icon_ball.svg'
+import sebrae from '../../../assets/images/home/sebrae.svg'
+import hostinger from '../../../assets/images/home/hostinger.svg'
+import frd_logo from '../../../assets/images/home/frd_logo.svg'
+
 
 export const SiteCasesSection = () => {
-    return(
+    return (
         <section>
-            <CardSiteSecondary
-            imagem={forbes_logo}
-            titulo="MachineLearningProject"
-            icone_seta={icone_botao_seta}
-            texto="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since"
-            icone_bola={icon_ball}
-            item1="Lorem Ipsum is simply "
-            item2="Lorem Ipsum is simply "
-            item3="Lorem Ipsum is simply "
-            item4="Lorem Ipsum is simply "/>
+            <h1>Estudos de caso</h1>
+            <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since</h2>
+            <div>
+                <CardSiteSecondary
+                    imagem={forbes_logo}
+                    titulo="MachineLearningProject"
+                    icone_seta={icone_botao_seta}
+                    texto="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since"
+                    icone_bola={icon_ball}
+                    item1="Lorem Ipsum is simply "
+                    item2="Lorem Ipsum is simply "
+                    item3="Lorem Ipsum is simply "
+                    item4="Lorem Ipsum is simply " />
+
+                <CardSiteSecondary
+                    imagem={sebrae}
+                    titulo="MachineLearningProject"
+                    icone_seta={icone_botao_seta}
+                    texto="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since"
+                    icone_bola={icon_ball}
+                    item1="Lorem Ipsum is simply "
+                    item2="Lorem Ipsum is simply "
+                    item3="Lorem Ipsum is simply "
+                    item4="Lorem Ipsum is simply " />
+            </div>
+
+            <div>
+                <CardSiteSecondary
+                    imagem={hostinger}
+                    titulo="MachineLearningProject"
+                    icone_seta={icone_botao_seta}
+                    texto="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since"
+                    icone_bola={icon_ball}
+                    item1="Lorem Ipsum is simply "
+                    item2="Lorem Ipsum is simply "
+                    item3="Lorem Ipsum is simply "
+                    item4="Lorem Ipsum is simply " />
+
+                <CardSiteSecondary
+                    imagem={frd_logo}
+                    titulo="MachineLearningProject"
+                    icone_seta={icone_botao_seta}
+                    texto="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since"
+                    icone_bola={icon_ball}
+                    item1="Lorem Ipsum is simply "
+                    item2="Lorem Ipsum is simply "
+                    item3="Lorem Ipsum is simply "
+                    item4="Lorem Ipsum is simply " />
+            </div>
+
         </section>
     )
 }
