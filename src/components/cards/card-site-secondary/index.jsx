bee473fa80f985d48f21f066d0eeb54dd246ae2e
aandremeleti-hub <aandremeleti@gmail.com>
2025-12-17 23:37:31 -0300

    style: card site secondary structure stylization

--- src/components/cards/card-site-secondary/index.jsx
+++ src/components/cards/card-site-secondary/index.jsx
@@ -1,34 +1,37 @@
 import './style.css'
 
-export const CardSiteSecondary = ({imagem, titulo, icone_seta, texto, icone_bola, item1, item2, item3, item4 }) => {
+export const CardSiteSecondary = ({ imagem, titulo, icone_seta, texto, icone_bola, item1, item2, item3, item4 }) => {
     return (
-        <section>
-            <img src={imagem} alt="" />
-            <div>
-                <h2>{titulo}</h2>
-                <img src={icone_seta} alt="" />
+        <section className='content-secondary-card-site'>
+            <img className='secondary-card-image' src={imagem} alt="" />
+            <div className='box-title-secondary-card-site'>
+                <h2 className='title-secondary-card-site'>{titulo}</h2>
+                <img className='image-title-secondary-card-site' src={icone_seta} alt="" />
             </div>
-            <h3>{texto}</h3>
-            <div>
-                <div>
-                    <img src={icone_bola} alt="" />
-                    <p>{item1}</p>
+            <h3 className='text-secondary-card-site'>{texto}</h3>
+            <div className='box-secondary-card-site-items'>
+                <div className='secondary-card-site-items'>
+                    <div className='card-site-left-items'>
+                        <img src={icone_bola} alt="" />
+                        <p>{item1}</p>
+                    </div>
+                    <div className='card-site-right-items'>
+                        <img src={icone_bola} alt="" />
+                        <p>{item2}</p>
+                    </div>
                 </div>
-                <div>
-                    <img src={icone_bola} alt="" />
-                    <p>{item2}</p>
+                <div className='secondary-card-site-items'>
+                    <div className='card-site-left-items'>
+                        <img src={icone_bola} alt="" />
+                        <p>{item3}</p>
+                    </div>
+                    <div className='card-site-right-items'>
+                        <img src={icone_bola} alt="" />
+                        <p>{item4}</p>
+                    </div>
                 </div>
             </div>
-            <div>
-                <div>
-                    <img src={icone_bola} alt="" />
-                    <p>{item3}</p>
-                </div>
-                <div>
-                    <img src={icone_bola} alt="" />
-                    <p>{item4}</p>
-                </div>
-            </div>
+
         </section>
     )
 }
--- src/components/layout/site_cases_section/index.jsx
+++ src/components/layout/site_cases_section/index.jsx
@@ -1,10 +1,22 @@
 import { CardSiteSecondary } from '../../cards/card-site-secondary'
 import './style.css'
+import forbes_logo from '../../../assets/images/home/forbes_logo.svg'
+import icone_botao_seta from '../../../assets/images/home/icone_botao_seta.svg'
+import icon_ball from '../../../assets/images/home/icon_ball.svg'
 
 export const SiteCasesSection = () => {
     return(
         <section>
-            <CardSiteSecondary/>
+            <CardSiteSecondary
+            imagem={forbes_logo}
+            titulo="MachineLearningProject"
+            icone_seta={icone_botao_seta}
+            texto="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since"
+            icone_bola={icon_ball}
+            item1="Lorem Ipsum is simply "
+            item2="Lorem Ipsum is simply "
+            item3="Lorem Ipsum is simply "
+            item4="Lorem Ipsum is simply "/>
         </section>
     )
 }
