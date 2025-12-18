1fac9ac8b6e57d35fd87bf8abfb901d876c21f07
aandremeleti-hub <aandremeleti@gmail.com>
2025-12-18 01:01:06 -0300

    fix: standardize icon size and behavior in main section

--- src/components/layout/design_main_section/index.jsx
+++ src/components/layout/design_main_section/index.jsx
@@ -2,14 +2,16 @@ import "./style.css"
 import design_icon from '../../../assets/images/home/design_icon.svg'
 
 export const DesignMainSection = () => {
-    return(
-        <div className="content_design_main_section">
-            <div className="container_title_main_section_design">
-                <h1>Design</h1>
-                <p>Learn for free about math, art, computer programming, economics, physics, chemistry, biology, medicine, finance, history.</p>
-            </div>
-            <img src={design_icon} alt="" />
-            <h2>Learn for free about math, art, computer programming, economics, physics, chemistry, biology, medicine, finance, history, and more. Learn for free about math, art, computer programming, economics, physics, chemistry, biology, medicine, finance, history.</h2>
+  return (
+    <section className="container-design-main-section">
+      <div className="content_design_main_section">
+        <div className="container_title_main_section_design">
+          <h1>Design</h1>
+          <p>Learn for free about math, art, computer programming, economics, physics, chemistry, biology, medicine, finance, history.</p>
         </div>
-    )
+        <img src={design_icon} alt="" />
+        <h2>Learn for free about math, art, computer programming, economics, physics, chemistry, biology, medicine, finance, history, and more. Learn for free about math, art, computer programming, economics, physics, chemistry, biology, medicine, finance, history.</h2>
+      </div>
+    </section>
+  )
 }
