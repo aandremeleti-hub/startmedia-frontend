fcbbed8282a3b1d071c406e2a7879c57be7a6200
aandremeleti-hub <aandremeleti@gmail.com>
2025-12-18 19:21:02 -0300

    feat: ia primary cards section structure creation

---
+++ src/components/layout/ia_primary_cards_section/index.jsx
@@ -0,0 +1,15 @@
+import { CardIaPrimary } from '../../cards/card-ia-primary'
+import './style.css'
+import dashboard_ia_primary_cards_section from '../../../assets/images/home/dashboard_ia_primary_cards_section.svg'
+
+
+export const IaPrimaryCardsSection = () => {
+    return(
+        <section className='ia-primary-cards-section'>
+            <CardIaPrimary
+            imagem={dashboard_ia_primary_cards_section}
+            titulo="Atendimento 24h"
+            texto="O cliente consegue resolver tudo rápido, sem esperar, sem burocracia, sem repetir informações. Isso aumenta satisfação e conversão."/>
+        </section>
+    )
+}
--- src/pages/ia/index.jsx
+++ src/pages/ia/index.jsx
@@ -1,11 +1,13 @@
 import { Header } from "../../components/layout/header"
 import { IaMainSection } from "../../components/layout/ia_main_section"
+import { IaPrimaryCardsSection } from "../../components/layout/ia_primary_cards_section"
 
 export const Ia = () => {
     return (
         <>
             <Header/>
             <IaMainSection/>
+            <IaPrimaryCardsSection/>
         </>
     )
 }
