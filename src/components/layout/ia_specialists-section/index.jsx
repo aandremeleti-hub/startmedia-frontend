4d17350e4b3e886ae271fc49ea2eaa01e34e6656
aandremeleti-hub <aandremeleti@gmail.com>
2025-12-19 11:13:01 -0300

    feat: ia specialists section structure creation

---
+++ src/components/layout/ia_specialists-section/index.jsx
@@ -0,0 +1,10 @@
+import { CardIaThird } from '../../cards/card-ia-third'
+import './style.css'
+
+export const IaSpecialistsSection = () => {
+    return(
+        <section>
+            <CardIaThird/>
+        </section>
+    )
+}
