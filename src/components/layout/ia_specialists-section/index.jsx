328f27e936ca7217d741a277240b8672eb22595e
aandremeleti-hub <aandremeleti@gmail.com>
2025-12-19 17:16:52 -0300

    feat: update ia specialists card section structure

--- src/components/layout/ia_specialists-section/index.jsx
+++ src/components/layout/ia_specialists-section/index.jsx
@@ -4,14 +4,31 @@ import line from '../../../assets/images/home/line.svg'
 import quotation_marks from '../../../assets/images/home/quotation_marks.svg'
 
 export const IaSpecialistsSection = () => {
-    return(
-        <section className='content-ia-specialists-section'>
-            <CardIaThird
-            linha={line}
-            aspas={quotation_marks}
-            titulo="A IA é o maior maremoto tecnológico desde a chegada da internet."
-            nome="Satya Nadella"
-            cargo="CEO da Microsoft"/>
+    return (
+        <section >
+            <div className='content-ia-specialists-section'>
+                <h1 className='title-ia-third-cards'>O que dizem os especialistas?</h1>
+                <div className='container-ia-third-cards'>
+                    <CardIaThird
+                        linha={line}
+                        aspas={quotation_marks}
+                        titulo="A IA é o maior maremoto tecnológico desde a chegada da internet."
+                        nome="Satya Nadella"
+                        cargo="CEO da Microsoft" />
+                    <CardIaThird
+                        linha={line}
+                        aspas={quotation_marks}
+                        titulo="A IA é o maior maremoto tecnológico desde a chegada da internet."
+                        nome="Satya Nadella"
+                        cargo="CEO da Microsoft" />
+                    <CardIaThird
+                        linha={line}
+                        aspas={quotation_marks}
+                        titulo="A IA é o maior maremoto tecnológico desde a chegada da internet."
+                        nome="Satya Nadella"
+                        cargo="CEO da Microsoft" />
+                </div>
+            </div>
         </section>
     )
 }
